81aa7dbeda0fcf1890b45cf302a8f968e1fc6127
Giovanni Rufino <giovannirufino@gmail.com>
2021-05-22 08:56:45 -0400

    07 done

--- 07_multiple_letter_count/multiple_letter_count.py
+++ 07_multiple_letter_count/multiple_letter_count.py
@@ -7,3 +7,7 @@ def multiple_letter_count(phrase):
         >>> multiple_letter_count('Yay')
         {'Y': 1, 'a': 1, 'y': 1}
     """
+    letter_freq  = {};
+    for letter in phrase:
+        letter_freq[letter] = letter_freq.get(letter, 0) + 1;
+    return letter_freq
